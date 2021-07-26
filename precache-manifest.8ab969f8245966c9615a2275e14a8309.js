a03b619f5f184878b6ed888fe54542e82efa0b29
Farid Shiraliyev <79901122+faridalz@users.noreply.github.com>
2021-07-26 18:47:33 +0400

    Updates

---
+++ precache-manifest.8ab969f8245966c9615a2275e14a8309.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f946b0cbb1d657d651c3b08d3eff4d",
+    "url": "/personal/index.html"
+  },
+  {
+    "revision": "91e97c8ec13f26866d8e",
+    "url": "/personal/static/css/2.3ea67ca0.chunk.css"
+  },
+  {
+    "revision": "ba5f72bb852ccae9b93f",
+    "url": "/personal/static/css/main.075c282c.chunk.css"
+  },
+  {
+    "revision": "91e97c8ec13f26866d8e",
+    "url": "/personal/static/js/2.e9a0db55.chunk.js"
+  },
+  {
+    "revision": "e713d55335c54bb185bd891924100104",
+    "url": "/personal/static/js/2.e9a0db55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba5f72bb852ccae9b93f",
+    "url": "/personal/static/js/main.6ea62e64.chunk.js"
+  },
+  {
+    "revision": "f13ee5e87857370d5f6a",
+    "url": "/personal/static/js/runtime-main.91b031ba.js"
+  },
+  {
+    "revision": "f5144680075f48fb4613b862cbf57a32",
+    "url": "/personal/static/media/Altair.f5144680.PNG"
+  },
+  {
+    "revision": "b957a93dd4b4454deb47526cd5b08bcf",
+    "url": "/personal/static/media/circle-cropped.b957a93d.png"
+  },
+  {
+    "revision": "5ae1004694da8deec0da40f0444f14b6",
+    "url": "/personal/static/media/email.5ae10046.png"
+  },
+  {
+    "revision": "7485d282284b105074756c5426305478",
+    "url": "/personal/static/media/facebook.7485d282.png"
+  },
+  {
+    "revision": "89c2c0674bdba3d61daf6b1b1e5e82b5",
+    "url": "/personal/static/media/github.89c2c067.png"
+  },
+  {
+    "revision": "85850624de95772a0590cc94f1436036",
+    "url": "/personal/static/media/landing.85850624.png"
+  },
+  {
+    "revision": "e4a9fd80020480628dd3a0f2d4a0593b",
+    "url": "/personal/static/media/linkedin.e4a9fd80.png"
+  },
+  {
+    "revision": "05865c92b229fe1c19e2806ca9d64907",
+    "url": "/personal/static/media/phone.05865c92.png"
+  },
+  {
+    "revision": "cb2d0352f182f150e2ba1b5c19b77447",
+    "url": "/personal/static/media/realkobe.cb2d0352.png"
+  },
+  {
+    "revision": "8846c41fdf096feca324805f8b243d49",
+    "url": "/personal/static/media/swe.8846c41f.png"
+  },
+  {
+    "revision": "ec7db6e6ce0362ac9314b030cba5c3bb",
+    "url": "/personal/static/media/zhu11.ec7db6e6.PNG"
+  }
+]);
